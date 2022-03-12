a4573e0aa38448ba6bbeb8984b085b6e5066b33d
Engineer in Bay Area <34151463+paulvalderama@users.noreply.github.com>
2022-03-12 14:39:22 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const session = require("express-session");
 const grant = require("grant").express();
@@ -13,12 +11,14 @@ app.use(session({ secret: "grant" }));
 app.use(express.static("./"));
 
 // mount grant
-
 app.use(
   grant({
+    //origin where client server can be reached
+    //login navigating to Redirect URI origin + "/connect/dexcom/callback"
     defaults: {
       origin: "https://dexcom-project1.herokuapp.com",
     },
+    //authorization parameters for dexcom api, using DEXCOM_CLIENT_ID / DEXCOM_SECRET
     dexcom: {
       authorize_url: "https://sandbox-api.dexcom.com/v2/oauth2/login",
       access_url: "https://sandbox-api.dexcom.com/v2/oauth2/token",
@@ -32,25 +32,17 @@ app.use(
 
 app.get("/", (req, res) => {
 
-    // res.send(` client id: ${process.env.DEXCOM_CLIENT_ID}, secret: ${process.env.DEXCOM_SECRET}`)
     // res.json(process.env)
-    // res.send(`
     
-    //   <h1>Your access token</h1>
-    //     <pre> ${accessToken} </pre>
-    // `);
-    
-    // console.log({
-    //     key: process.env.DEXCOM_CLIENT_ID,
-    //     secret: process.env.DEXCOM_SECRET,
-    // });
-
+    //retrieve accesstoken
     const accessToken = req.query.access_token;
     
+    //redirect if accesstoken invalid
     if(!accessToken){
         return res.redirect('/connect/dexcom')
     } 
     
+    //fetch sandboxapi, with 2 weeks of data 2/1/22 - 2/14/222
     fetch('https://sandbox-api.dexcom.com/v2/users/self/egvs?startDate=2022-02-01T00:00:00&endDate=2022-02-14T23:59:00', {
         method: "GET",
         headers: {
@@ -78,7 +70,7 @@ app.get("/", (req, res) => {
         let gmiRounded = gmi.toFixed(2)
 
         
-        res.send(`<h1>GMI for last 14 days: ${gmiRounded}</h1>`)
+        res.send(`<h1>GMI for last 14 days : ${gmiRounded}</h1>`)
     })
     .catch((error) => {
         console.log(error)
